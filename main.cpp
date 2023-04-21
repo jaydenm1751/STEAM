806788f230d656a15154624b4b209044c0d23adb
Jayden McKenna <jaydenmckenna@ufl.edu>
2023-04-20 22:02:14 -0400

    SFML window. Backspace does not work. Need to center text as well

--- main.cpp
+++ main.cpp
@@ -2,8 +2,47 @@
 #include "HashTable.h"
 #include "PriorityQ.h"
 #include <SFML/Graphics.hpp>
+#include <chrono>
 
 using namespace std;
+//TODO: Add Nodes to the list from .CSV
+void InitializeMap(string fileName, HashTable& catalogue) {
+    auto start_time = chrono::steady_clock::now();
+
+    string path = "files/" + fileName + ".csv";
+    ifstream storeFile(path);
+
+    while (true) {
+        if (!storeFile.is_open()) {
+            cerr << "Error opening the file:" << path << endl;
+        } else {
+            string headerLine;
+            getline(storeFile, headerLine);
+
+            string line;
+            while(getline(storeFile, line)) {
+                istringstream line_stream(line);
+                string cell;
+
+                while (getline(line_stream, cell, ',')) {
+                    if(cell.empty()) {
+                        cout << "N/A\t";
+                    }else {
+                        cout << cell << "\t";
+                    }
+                }
+                cout << endl;
+            }
+            storeFile.close();
+            break;
+        }
+    }
+
+    cout << endl;
+    auto end_time = chrono::steady_clock::now();
+    auto elapsed_time = chrono::duration_cast<chrono::seconds>(end_time - start_time);
+    cout << "Elapsed time: " << elapsed_time.count() << " sec.\n";
+}
 
 int main() {
     HashTable map;
