cc4b4a2adc5098e59890bc3b9c628820eb19d7a7
Jayden McKenna <jaydenmckenna@ufl.edu>
2023-04-23 20:30:46 -0400

    title search GUI feature built needs backend dev

--- StorePage.cpp
+++ StorePage.cpp
@@ -113,6 +113,8 @@ static void makeGUI() {
     unordered_map < string, ConsoleNode * > ConsoleGames;
     unordered_map < string, AppNode * > AppGames;
 
+    InitializeMapConsole(ConsoleGames);
+
     int width = 1500;
     int height = 850;
 
@@ -123,11 +125,16 @@ static void makeGUI() {
     font.loadFromFile("files/CourierPrime-Regular.ttf");
     sf::Text cursor;
     makeText(cursor, font, "|", 20, (height / 2) + 100, 200);
+    sf::Text cursor2;
+    makeText(cursor2, font, "|", 20, 0, 0);
     sf::String input = "";
     sf::Text waterVaporText;
     makeText(waterVaporText, font, "WaterVapor Gaming", 60, height / 2, 100);
     sf::Text initialQ;
     makeText(initialQ, font, "How many parameters would you like to search by?", 35, 325, 200);
+    sf::Text sByTitle;
+    makeText(sByTitle, font, "Enter Title here: ", 35, 50, 220);
+
     int numHeight = 240;
     vector <sf::Text> initialNumbers;
     for (int i = 1; i <= 4; i++) {
@@ -155,6 +162,18 @@ static void makeGUI() {
     sf::Text searchParameterDisplay;
     string searchParameter1;
 
+    sf::RectangleShape titleBox(sf::Vector2f(300, 60));
+    titleBox.setPosition(50, 273);
+    titleBox.setFillColor(sf::Color::White);
+    titleBox.setOutlineThickness(2);
+    titleBox.setOutlineColor(sf::Color::Black);
+    bool isEditing = false;
+    string titleSearch;
+    sf::Sprite star;
+    star = sf::Sprite(TextureManager::GetTexture("star"));
+    star.setPosition(370, 276);
+    star.setScale(0.25, 0.25);
+    bool displayStar = false;
 
     sf::Sprite chief;
     chief = sf::Sprite(TextureManager::GetTexture("chief"));
@@ -202,29 +221,39 @@ static void makeGUI() {
                     searchParams.clear();
                     numBoxes = 1;
                     input.clear();
-                    cursor.setString("|");
+                    cursor.setString("");
                     cursor.setPosition(525, 200);
+                    cursor2.setString("");
+                    cursor2.setPosition(0,0);
                     allowTextInput = true;
+                    displayStar = false;
+                }
+                if (titleBox.getGlobalBounds().contains(coordinates.x, coordinates.y)){
+                    isEditing = true;
                 }
                 if (coal.getGlobalBounds().contains(coordinates.x, coordinates.y)) { //option 1
                     maxNumBoxes = 1;
                     parametersQ = true;
+                    isEditing = false;
                 }
                 if (iron.getGlobalBounds().contains(coordinates.x, coordinates.y)) { // option 2
                     maxNumBoxes = 2;
                     parametersQ = true;
+                    isEditing = false;
                 }
                 if (gold.getGlobalBounds().contains(coordinates.x, coordinates.y)) { //option 3
                     maxNumBoxes = 3;
                     parametersQ = true;
+                    isEditing = false;
                 }
                 if (diamond.getGlobalBounds().contains(coordinates.x, coordinates.y)) { //option 4
                     maxNumBoxes = 4;
                     parametersQ = true;
+                    isEditing = false;
                 }
             }
-            if (parametersQ) {
-                if (event.type == sf::Event::TextEntered && !entered && allowTextInput) {
+            if (parametersQ || isEditing) {
+                if (event.type == sf::Event::TextEntered && !entered && allowTextInput && !isEditing) {
                     if ((isalpha(event.text.unicode) || isdigit(event.text.unicode) || ispunct(event.text.unicode)
                          || isspace(event.text.unicode)) && input.getSize() < 25) {
                         input += event.text.unicode;
@@ -234,12 +263,42 @@ static void makeGUI() {
                         cursor.setPosition(525, x);
                     }
 
-                } else if (event.type == sf::Event::KeyPressed) {
-                    if (event.key.code == sf::Keyboard::BackSpace) {
+                } if (isEditing && event.type == sf::Event::TextEntered) {
+                    if ((isalpha(event.text.unicode) || isdigit(event.text.unicode) || ispunct(event.text.unicode)
+                         || isspace(event.text.unicode)) && input.getSize() < 50) {// Check if ASCII and not backspace
+                        input += event.text.unicode;
+                        if (input.getSize() <= 25) {
+                            cursor.setString(input + "|");
+                            cursor.setPosition(50, 273);
+                            if (input.getSize() == 25){
+                                cursor.setString(input);
+                            }
+                        } else if (input.getSize() < 50){
+//                            cursor.setString(input);
+//                            cursor.setPosition(50, 273);
+                            cursor2.setString(input.substring(25) + "|");
+                            cursor2.setPosition(50, 293);
+                        }
+                    }
+
+                } if (event.type == sf::Event::KeyPressed) {
+                    if (!isEditing && event.key.code == sf::Keyboard::BackSpace) {
                         input = input.substring(0, input.getSize() - 1);
                         cursor.setString(input + "|");
                     }
-                    if (event.key.code == sf::Keyboard::Enter && numBoxes <= maxNumBoxes && allowTextInput) {
+                    if (isEditing && event.key.code == sf::Keyboard::BackSpace){
+                        input = input.substring(0, input.getSize() - 1);
+                        if (input.getSize() >= 25) {
+                            cursor2.setString(input.substring(25) + "|");
+                            if (input.getSize() == 25){
+                                cursor2.setString(input.substring(25));
+                            }
+                        }
+                        if (input.getSize() < 25){
+                            cursor.setString(input + "|");
+                        }
+                    }
+                    if (!isEditing && event.key.code == sf::Keyboard::Enter && numBoxes <= maxNumBoxes && allowTextInput) {
                         searchParameter1 = input.toAnsiString();
                         searchParams.push_back(input);
                         numBoxes++;
@@ -256,8 +315,21 @@ static void makeGUI() {
                             transform(searchParameter1.begin(), searchParameter1.end(), searchParameter1.begin(),
                                       [](unsigned char c) { return std::tolower(c); });
                             cout << searchParameter1 << endl;
-
                         }
+                    }
+
+                    //TODO: should i make the text giant and bolded like the other things or naw
+                    if (isEditing && event.key.code == sf::Keyboard::Enter && allowTextInput) {
+                        titleSearch = input.toAnsiString();
+                        if (!titleSearch.empty()){
+                            transform(titleSearch.begin(), titleSearch.end(), titleSearch.begin(),
+                                      [](unsigned char c) { return std::tolower(c); });
+                            displayStar = true;
+                            isEditing = false;
+                        }
+                        input.clear();
+                        cursor.setString("");
+                        cursor2.setString("");
                     }
                 }
             }
@@ -265,6 +337,18 @@ static void makeGUI() {
         // order should be clear, draw display.
         window.clear(sf::Color::Blue);
         window.draw(waterVaporText);
+
+        //search by title
+        window.draw(sByTitle);
+        window.draw(titleBox);
+        if(isEditing && !parametersQ){
+            window.draw(cursor);
+            window.draw(cursor2);
+        }
+        if (displayStar){
+            window.draw(star);
+        }
+
         int x = 200;
         if (parametersQ) {
             for (int i = 1; i <= numBoxes; i++) {
